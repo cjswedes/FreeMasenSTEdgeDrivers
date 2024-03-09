53698111cae5f50428656168dd61cb96fbfd51c5
Carter Swedal <carter.swedal@smartthings.com>
2024-03-08 19:26:47 -0700

    Setup various subdriver configurations

---
+++ subdriver-stress-test/src/config.lua
@@ -0,0 +1,5 @@
+return {
+	NUM_DEVICES = 10,
+	--"UNIFORM" or "LAST_ONLY" or "FIRST_ONLY" or "NONE" are the options
+	CAN_HANDLE_DISTRIBUTION = "FIRST_ONLY" 
+}
--- subdriver-stress-test/src/init.lua
+++ subdriver-stress-test/src/init.lua
@@ -3,10 +3,9 @@ local log = require 'log'
 local capabilities = require "st.capabilities"
 local cosock = require "cosock"
 local socket = cosock.socket
-local st_device = require "st.device"
-local MsgDispatcher = require "dispatcher"
+local config = require "config"
+local utils = require "st.utils"
 
-local NUM_DEVICES = 10
 local function disco(driver, opts, cont)
   print('starting disco', cont)
   local device_list = driver.device_api.get_device_list()
@@ -22,8 +21,8 @@ local function disco(driver, opts, cont)
     }
     driver:try_create_device(device_info)
     socket.sleep(0.2)
-    for i=1, NUM_DEVICES do
-      local id = string.format("device %s", i)
+    for i=1, config.NUM_DEVICES do
+      local id = string.format("device %s", i) -- DO NOT CHANGE use in subdriver can_handle functions
       local device_info = {
         type = 'LAN',
         device_network_id = id,
@@ -40,49 +39,23 @@ local function disco(driver, opts, cont)
   log.debug('disco over', continues)
 end
 
-local function handle_off(driver, device, cmd)
-  device.log.info("base: handle_off")
-  device:emit_event(capabilities.switch.switch.off())
-end
-local function handle_on(driver, device, cmd)
-  device.log.info("base: handle_on")
-  device:emit_event(capabilities.switch.switch.on())
-end
-local function handle_level(driver, device, cmd)
-  device.log.info("base: handle_level")
-  device:emit_event(capabilities.switchLevel.level(cmd.args.level))
-end
-local function handle_refresh(driver, device, cmd)
-  device.log.info("base: handle_refresh")
-end
-
 local function device_added(driver, device)
   device:emit_event(capabilities.switch.switch.on())
   device:emit_event(capabilities.switchLevel.level(0))
   device:online()
 end
 
+local subdrivers = require "subdrivers"
+
 local driver = Driver('Subdriver Stress Test', {
   discovery = disco,
   lifecycle_handlers = {
     added = device_added,
   },
-  capability_handlers = {
-    [capabilities.switch.ID] = {
-      [capabilities.switch.commands.on.NAME] = handle_on,
-      [capabilities.switch.commands.off.NAME] = handle_off,
-    },
-    [capabilities.switchLevel.ID] = {
-      [capabilities.switchLevel.commands.setLevel.NAME] = handle_level
-    },
-    [capabilities.refresh.ID] = {
-      [capabilities.refresh.commands.refresh.NAME] = handle_refresh,
-    },
-  },
-  subdrivers = {
-
-  },
+  capability_handlers = require("capability_handlers"),
+  sub_drivers = subdrivers
 })
+
 
 log.debug('Starting lan parent child driver')
 driver:run()
