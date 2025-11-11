6e7379ae313059d904e96551b8e4ebaa59dc2d02
Carter Swedal <carter.swedal@smartthings.com>
2025-11-11 14:10:44 -0600

    Fix lan-test-driver to have an arbitrary number of devices created

--- lan-test-driver/src/init.lua
+++ lan-test-driver/src/init.lua
@@ -2,44 +2,41 @@ local Driver = require 'st.driver'
 local log = require 'log'
 local capabilities = require "st.capabilities"
 local socket = require "cosock.socket"
--- local socket = require "cosock.socket"
--- local cosock = require "cosock"
--- local st_device = require "st.device"
--- local utils = require "st.utils"
 
 local function disco(driver, opts, cont)
+  local NUM_DEVICES = 40
   print('starting disco', cont)
   local device_list = driver.device_api.get_device_list()
   if not next(device_list) and cont() then
-    print('discovering a device')
-    local device_info = {
+    print('discovering a devices')
+    for i = 0, NUM_DEVICES do
+      local device_info = {
         type = 'LAN',
-        device_network_id = string.format('parent-%s', os.time()),
-        label = 'lan-device',
+        device_network_id = string.format('%s', i),
+        label = string.format('lan-device-%s', i),
         profile = 'basic',
         manufacturer = "asdf",
         model = "fdsa",
-        vendor_provided_label = 'parent',
+        vendor_provided_label = 'ffff',
     }
     driver:try_create_device(device_info)
+    socket.sleep(0.25)
+    end
   end
-  log.debug('disco over', continues)
+  log.debug('disco over', cont())
 end
 
 local function handle_off(driver, device, cmd)
   log.info("handle_off")
   device:emit_event(capabilities.switch.switch.off())
-  driver.tcp_sock = socket.tcp()
 end
 local function handle_on(driver, device, cmd)
   log.info("handle_on")
   device:emit_event(capabilities.switch.switch.on())
-  driver.udp_sock = socket.udp()
 end
 local function handle_level(driver, device, cmd)
   log.info("handle_level")
   device:emit_event(capabilities.switchLevel.level(cmd.args.level))
-  driver.zwave_sock = socket.zwave()
 end
 
 local function handle_refresh(driver, device, cmd)
@@ -49,10 +46,9 @@ end
 local function device_added(driver, device)
   device:emit_event(capabilities.switch.switch.on())
   device:emit_event(capabilities.switchLevel.level(0))
-
 end
 
-local driver = Driver('Lan Child Test', {
+local driver = Driver('Lan test driver', {
   discovery = disco,
   lifecycle_handlers = {
     added = device_added,
@@ -68,7 +64,8 @@ local driver = Driver('Lan Child Test', {
     [capabilities.refresh.ID] = {
       [capabilities.refresh.commands.refresh.NAME] = handle_refresh,
     },
-}
+  },
+  use_single_devices_thread = true,
 })
 
 log.debug('Starting lan parent child driver')
